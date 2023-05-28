add9af2b0baefe0afb8c5d9b3cbc9ddeb9771897
Nio Azizudin <io.rvrse@gmail.com>
2023-05-29 05:27:36 +0700

    fix link for dashboard

--- navigation.php
+++ navigation.php
@@ -1,5 +1,5 @@
 <ul>
-    <li><a href="#">Dashboard</a></li>
+    <li><a href="index.php">Dashboard</a></li>
     <li><a href="dosen.php">Database Dosen</a></li>
     <li><a href="semester.php">Database Semester</a></li>
     <li><a href="slot_waktu.php">Database Slot Waktu</a></li>
