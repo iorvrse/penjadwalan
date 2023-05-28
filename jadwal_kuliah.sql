a963d907348c1f7a54f6c5ac6558859c8068397f
sakananda <134681056+sakananda@users.noreply.github.com>
2023-05-28 22:58:09 +0700

    jadwal_kuliah.sql

--- jadwal_kuliah.sql
+++ jadwal_kuliah.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: May 26, 2023 at 09:57 AM
+-- Generation Time: May 28, 2023 at 03:56 PM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.26
 
@@ -30,13 +30,13 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `dosen`;
 CREATE TABLE IF NOT EXISTS `dosen` (
   `id_dosen` int NOT NULL AUTO_INCREMENT,
-  `nama` varchar(100) NOT NULL,
+  `nama` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
   `nip` int NOT NULL,
-  `bidang_ilmu` varchar(50) NOT NULL,
+  `bidang_ilmu` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   `id_pengguna` int NOT NULL,
   PRIMARY KEY (`id_dosen`),
   KEY `id_pengguna` (`id_pengguna`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -48,10 +48,10 @@ DROP TABLE IF EXISTS `jadwal`;
 CREATE TABLE IF NOT EXISTS `jadwal` (
   `id_jadwal` int NOT NULL AUTO_INCREMENT,
   `jumlah_mahasiswa` int NOT NULL,
-  `status_jadwal` varchar(50) NOT NULL,
-  `progress_jadwal` varchar(50) NOT NULL,
-  `id_semester` enum('1','2') NOT NULL,
-  `id_ruangan` varchar(10) NOT NULL,
+  `status_jadwal` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `progress_jadwal` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `id_semester` enum('1','2','3','4','5','6','7','8') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `id_ruangan` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
   `id_dosen` int NOT NULL,
   `id_slot` int NOT NULL,
   `kode_matakuliah` int NOT NULL,
@@ -72,12 +72,12 @@ CREATE TABLE IF NOT EXISTS `jadwal` (
 DROP TABLE IF EXISTS `matakuliah`;
 CREATE TABLE IF NOT EXISTS `matakuliah` (
   `kode_matakuliah` int NOT NULL AUTO_INCREMENT,
-  `nama_matakuliah` varchar(50) NOT NULL,
+  `nama_matakuliah` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   `semester` int NOT NULL,
-  `jenis_matakuliah` varchar(50) NOT NULL,
-  `flag_aktif` varchar(15) NOT NULL,
+  `jenis_matakuliah` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `flag_aktif` varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`kode_matakuliah`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -88,11 +88,10 @@ CREATE TABLE IF NOT EXISTS `matakuliah` (
 DROP TABLE IF EXISTS `pengguna`;
 CREATE TABLE IF NOT EXISTS `pengguna` (
   `id_pengguna` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `nama_pengguna` varchar(50) NOT NULL,
-  `level_pengguna` varchar(50) NOT NULL,
-  `flag_aktif` varchar(15) NOT NULL,
+  `username` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `nama_pengguna` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `level_pengguna` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id_pengguna`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -104,8 +103,8 @@ CREATE TABLE IF NOT EXISTS `pengguna` (
 
 DROP TABLE IF EXISTS `ruangan`;
 CREATE TABLE IF NOT EXISTS `ruangan` (
-  `id_ruangan` int NOT NULL AUTO_INCREMENT,
-  `nama_ruangan` varchar(20) NOT NULL,
+  `id_ruangan` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
+  `nama_ruangan` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
   `kapasitas_ruangan` int NOT NULL,
   PRIMARY KEY (`id_ruangan`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -118,10 +117,11 @@ CREATE TABLE IF NOT EXISTS `ruangan` (
 
 DROP TABLE IF EXISTS `semester`;
 CREATE TABLE IF NOT EXISTS `semester` (
-  `id_semester` int NOT NULL,
-  `nama_semester` varchar(50) NOT NULL,
+  `id_semester` enum('1','2','3','4','5','6','7','8') COLLATE utf8mb4_general_ci NOT NULL,
+  `nama_semester` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   `tgl_awal_semester` date NOT NULL,
-  `tgl_akhir_semester` date NOT NULL
+  `tgl_akhir_semester` date NOT NULL,
+  PRIMARY KEY (`id_semester`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -135,9 +135,29 @@ CREATE TABLE IF NOT EXISTS `slot_waktu` (
   `id_slot` int NOT NULL AUTO_INCREMENT,
   `waktu_slot_awal` time NOT NULL,
   `waktu_slot_akhir` time NOT NULL,
-  `hari_slot` varchar(10) NOT NULL,
+  `hari_slot` varchar(10) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id_slot`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `dosen`
+--
+ALTER TABLE `dosen`
+  ADD CONSTRAINT `dosen_ibfk_1` FOREIGN KEY (`id_pengguna`) REFERENCES `pengguna` (`id_pengguna`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `jadwal`
+--
+ALTER TABLE `jadwal`
+  ADD CONSTRAINT `jadwal_ibfk_1` FOREIGN KEY (`id_semester`) REFERENCES `semester` (`id_semester`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `jadwal_ibfk_2` FOREIGN KEY (`id_ruangan`) REFERENCES `ruangan` (`id_ruangan`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `jadwal_ibfk_3` FOREIGN KEY (`id_slot`) REFERENCES `slot_waktu` (`id_slot`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `jadwal_ibfk_4` FOREIGN KEY (`kode_matakuliah`) REFERENCES `matakuliah` (`kode_matakuliah`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `jadwal_ibfk_5` FOREIGN KEY (`id_dosen`) REFERENCES `dosen` (`id_dosen`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
